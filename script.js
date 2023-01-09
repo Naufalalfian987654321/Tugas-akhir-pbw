27d0e4d7bad7b2d9be241645efa0a986d18fbb1d
naufalalfian987654321 <naufalalfian125@gmail.com>
2023-01-09 09:41:32 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var tombolMenu = document.getElementsByClassName('tombol-menu')[0];
+var menu = document.getElementsByClassName('menu')[0];
+
+tombolMenu.onclick = function() {
+    menu.classList.toggle('active');
+}
+
+menu.onclick = function() {
+    menu.classList.toggle('active');
+}
+/* Toggle antara menambahkan dan menghapus kelas "responsif" ke topnav saat pengguna mengklik ikon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
